3acddea88f3149a41e790c5bd88481d0bb894778
rvnd96 <rvndmadushan00@gmail.com>
2023-12-06 21:37:50 +0530

    main section and contact section added

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,38 @@ $(document).ready(function () {
     // let navLinks = document.querySelectorAll('header nav a');
     window.onscroll = () => {
         scrollFunction();
-
+        buttonScrollFunc();
     };
 
     function scrollFunction() {
         if (
-            document.body.scrollTop > 80 ||
-            document.documentElement.scrollTop > 80
+            document.body.scrollTop > 60 ||
+            document.documentElement.scrollTop > 60
         ) {
             $('.navbar').addClass('navbar-dropshadow');
         } else {
             $('.navbar').removeClass('navbar-dropshadow');
         }
     }
+    const gotoTopBtn = $('#topBtn');
+    const buttonScrollFunc = () => {
+        if (
+            document.body.scrollTop > 300 ||
+            document.documentElement.scrollTop > 300
+        ) {
+            gotoTopBtn.addClass('show')
+        } else {
+            gotoTopBtn.removeClass('show')
+        }
+    }
+
+    
+    const topBtnClick = () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+    gotoTopBtn.click(topBtnClick)
 
 });
 
@@ -26,3 +45,8 @@ $(document).ready(function () {
 //     dropRadius: 20,
 //     perturbance: 0.04,
 // })
+
+// go to btn
+
+
+
